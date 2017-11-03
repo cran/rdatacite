676fa2820fd136fad13f901047bd98e287f309b6
Scott Chamberlain <myrmecocystus@gmail.com>
2017-11-03 15:51:46 +0000

    version 0.3.0

--- tests/testthat/test-dc_search.R
+++ tests/testthat/test-dc_search.R
@@ -5,18 +5,20 @@ test_that("dc_search basic functionality works", {
   skip_on_cran()
 
   # default uses q=*:*
-  aa <- dc_search(verbose = FALSE)
+  aa <- dc_search(q = "*:*")
   # basic search
-  bb <- dc_search(q = "laser", rows = 2, verbose = FALSE)
+  #bb <- dc_search(q = "laser", rows = 2, verbose = FALSE)
   # specify fields to get back
-  bb <- dc_search(q = "laser", fl=c('doi','publicationYear'), rows=2, verbose = FALSE)
+  bb <- dc_search(q = "laser", fl=c('doi','publicationYear'),
+    rows=2)
   # search a specific field
-  cc <- dc_search(q = "subject:geology", fl=c('doi','subject'), rows=2, verbose = FALSE)
+  cc <- dc_search(q = "subject:geology", fl=c('doi','subject'),
+    rows=2)
 
   expect_is(aa, "tbl_df")
   expect_is(aa$minted, "character")
-  expect_is(aa$language, "character")
-  expect_less_than(20, NCOL(aa))
+  expect_is(aa$subject, "character")
+  expect_lt(20, NCOL(aa))
 
   expect_is(bb, "tbl_df")
   expect_named(bb, c('doi', 'publicationYear'))
@@ -30,8 +32,8 @@ test_that("dc_search basic functionality works", {
 test_that("dc_search works w/ csv output", {
   skip_on_cran()
 
-  aa <- dc_search(q = 'wind', fl=c('doi','title'), wt='csv', verbose = FALSE)
-  bb <- dc_search(q = 'wind', fl=c('doi','title'), verbose = FALSE)
+  aa <- dc_search(q = 'wind', fl=c('doi','title'), wt='csv')
+  bb <- dc_search(q = 'wind', fl=c('doi','title'))
 
   expect_is(aa, "tbl_df")
   expect_named(aa, c('doi', 'title'))
@@ -42,7 +44,6 @@ test_that("dc_search works w/ csv output", {
 
 test_that("dc_search fails nicely", {
   skip_on_cran()
-  library('httr')
-
-  expect_error(dc_search(callopts = timeout(0.01)), "Timeout was reached")
+  expect_error(dc_search(callopts = list(timeout_ms = 1)),
+    "Timeout was reached")
 })
