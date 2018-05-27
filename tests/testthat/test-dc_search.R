73c3f11df6fb7812cdba153b215c094ab32ce305
Scott Chamberlain <myrmecocystus@gmail.com>
2018-05-27 17:25:04 +0000

    version 0.4.0

--- tests/testthat/test-dc_search.R
+++ tests/testthat/test-dc_search.R
@@ -1,49 +1,39 @@
 context("dc_search")
 
-
 test_that("dc_search basic functionality works", {
-  skip_on_cran()
+  vcr::use_cassette("dc_search", {
+    # default uses q=*:*
+    aa <- dc_search(q = "*:*")
+    expect_is(aa, "tbl_df")
+    expect_is(aa$minted, "character")
+    expect_lt(20, NCOL(aa))
 
-  # default uses q=*:*
-  aa <- dc_search(q = "*:*")
-  # basic search
-  #bb <- dc_search(q = "laser", rows = 2, verbose = FALSE)
-  # specify fields to get back
-  bb <- dc_search(q = "laser", fl=c('doi','publicationYear'),
-    rows=2)
-  # search a specific field
-  cc <- dc_search(q = "subject:geology", fl=c('doi','subject'),
-    rows=2)
+    # basic search
+    # specify fields to get back
+    bb <- dc_search(q = "data", fl=c('doi','updated'),
+      rows=2)
 
-  expect_is(aa, "tbl_df")
-  expect_is(aa$minted, "character")
-  expect_is(aa$subject, "character")
-  expect_lt(20, NCOL(aa))
+    expect_is(bb, "tbl_df")
+    expect_named(bb, c('updated', 'doi'))
+    expect_equal(NROW(bb), 2)
 
-  expect_is(bb, "tbl_df")
-  expect_named(bb, c('doi', 'publicationYear'))
-  expect_equal(NROW(bb), 2)
-
-  expect_is(cc, "tbl_df")
-  expect_named(cc, c('doi', 'subject'))
-  expect_equal(NROW(cc), 2)
+    # search a specific field
+    cc <- dc_search(q = "datacentre:global", fl=c('doi','has_media'),
+      rows=2)
+    expect_is(cc, "tbl_df")
+    expect_named(cc, c('has_media', 'doi'))
+    expect_equal(NROW(cc), 2)
+  })
 })
 
 test_that("dc_search works w/ csv output", {
-  skip_on_cran()
+  vcr::use_cassette("dc_search_csv", {
+    aa <- dc_search(q = '*:*', fl=c('doi','title'), wt='csv')
+    bb <- dc_search(q = '*:*', fl=c('doi','title'))
 
-  aa <- dc_search(q = 'wind', fl=c('doi','title'), wt='csv')
-  bb <- dc_search(q = 'wind', fl=c('doi','title'))
+    expect_is(aa, "tbl_df")
 
-  expect_is(aa, "tbl_df")
-  expect_named(aa, c('doi', 'title'))
-
-  # csv and json output differ, some encoding problem likely
-  expect_false(identical(aa, bb))
-})
-
-test_that("dc_search fails nicely", {
-  skip_on_cran()
-  expect_error(dc_search(callopts = list(timeout_ms = 1)),
-    "Timeout was reached")
+    # csv and json output differ, some encoding problem likely
+    expect_false(identical(aa, bb))
+  })
 })
